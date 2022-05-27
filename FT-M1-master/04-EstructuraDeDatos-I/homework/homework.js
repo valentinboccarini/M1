eb58b70779e2037dba07d98b535fcf9573f0a29d
valentinboccarini <84943985+valentinboccarini@users.noreply.github.com>
2022-05-27 13:28:13 -0300

    04-Estructura-De-Datos-I

--- FT-M1-master/04-EstructuraDeDatos-I/homework/homework.js
+++ FT-M1-master/04-EstructuraDeDatos-I/homework/homework.js
@@ -3,25 +3,67 @@
 /*
 Definir las funciones recursivas nFactorial y nFibonacci.
 
-nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial (representado como n!) es el producto de n por todos los números naturales menores que él y mayores a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
+nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial (representado como n!)
+ es el producto de n por todos los números naturales menores que él y mayores a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
 
-nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
+nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente,
+ como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
 Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
 
 Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
 
 
-Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
+Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren
+ los mismos resultados pero de manera iterativa.
+*/
+/* DIJIMOS QUE PARA FACTOREAR ERA 4!
+0! = 1
+1! = 1
+2! = 2 * 1!
+3! = 3 * 2!
+4! = 3! * 4
+
+n = factorial -1 (uno menos que el valor de "n") * n
 */
 
 function nFactorial(n) {
-}
-
-function nFibonacci(n) {
+ if (n === 0 ||n === 1){ 
+   return 1;
+  }
+  return n * nFactorial(n - 1);
 }
 
 /*
-Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
+nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, 
+respectivamente,
+ como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta 
+ secuencia será el resultado de la suma del último elemento y el anterior.
+Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
+
+Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
+
+LA SECUENCIA FIBONACCI CONSISTE EN QUE EL NUMERO ES LA SUMA DE LOS DOS ANTERIORES
+nfibonacci(n - 2) + nFibonacci(n - 1)
+Ej:
+nFibonacci = [0,1]
+nfibonacci(n - 2) = 0 /// nfibonacci(n - 1) = 1
+              0        +        1     === 1
+            nFibonacci =[0,1,1]
+ */
+
+function nFibonacci(n) {
+  if (n === 0 || n === 1){
+    return n;
+  }
+  return nFibonacci(n - 2) + nFibonacci(n - 1);
+  
+
+  
+}
+
+/*
+Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa
+ es el primero que se quita. Definir los siguientes métodos:
   - enqueue: agrega un valor respetando el orden.
   - dequeue: remueve un valor respetando el orden. Retorna undefined cuando la queue está vacía.
   - size: retorna el tamaño (cantidad de elementos) de la queue.
@@ -29,10 +71,21 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {
-
+class Queue {
+  constructor(){
+    this.cadena = [];
+  }
+  enqueue(valor){
+    this.cadena.push(valor);
+  }
+  dequeue(valor){
+    return this.cadena.shift(valor);
+  }
+  size(){
+    return this.cadena.length;
+  }
+  
 }
-
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
